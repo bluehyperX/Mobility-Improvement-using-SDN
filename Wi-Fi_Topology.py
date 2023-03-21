628df3924caa10c3007976550f71e9e27e85a249
Sparsh Kumar <sparsh.kr99@protonmail.com>
2023-03-21 17:53:51 +0530

    Updated codes

--- Wi-Fi_Topology.py
+++ Wi-Fi_Topology.py
@@ -7,7 +7,6 @@ from mn_wifi.node import Station, OVSKernelAP
 from mn_wifi.cli import CLI
 from mn_wifi.link import wmediumd
 from mn_wifi.wmediumdConnector import interference
-from subprocess import call
 
 def myNetwork():
 
@@ -18,7 +17,8 @@ def myNetwork():
                     )
 
     info( '*** Adding controller\n' )
-    c0 = net.addController(name='c0', controller=RemoteController, port=6653)
+    controller_ = net.addHost('con', ip='10.0.0.100/8', inNamespace=False)
+    c0 = net.addController(name='c0', controller=RemoteController, ip='127.0.0.1', port=6653)
 
     info( '*** Adding switches/APs\n')
     s1 = net.addSwitch('s1', cls=OVSKernelSwitch)
@@ -26,15 +26,15 @@ def myNetwork():
     s3 = net.addSwitch('s3', cls=OVSKernelSwitch)
     s4 = net.addSwitch('s4', cls=OVSKernelSwitch)
     s5 = net.addSwitch('s5', cls=OVSKernelSwitch)
-    ap1 = net.addAccessPoint('ap1', cls=OVSKernelAP, ssid='ap1-ssid', channel='1', mode='g', position='300.0,400.0,0', range=200)
-    ap2 = net.addAccessPoint('ap2', cls=OVSKernelAP, ssid='ap2-ssid', channel='6', mode='g', position='1500.0,400.0,0', range=200)
-    ap3 = net.addAccessPoint('ap3', cls=OVSKernelAP, ssid='ap3-ssid', channel='11', mode='g', position='2700.0,400.0,0', range=200)
+    ap1 = net.addAccessPoint('ap1', cls=OVSKernelAP, mac='00:00:00:00:00:01', ssid='ap1-ssid', channel='1', mode='g', position='1500.0,2000.0,0', range=500)
+    ap2 = net.addAccessPoint('ap2', cls=OVSKernelAP, mac='00:00:00:00:00:02', ssid='ap2-ssid', channel='6', mode='g', position='3000.0,2000.0,0', range=500)
+    ap3 = net.addAccessPoint('ap3', cls=OVSKernelAP, mac='00:00:00:00:00:03', ssid='ap3-ssid', channel='11', mode='g', position='4200.0,2000.0,0', range=500)
 
     info( '*** Adding hosts/stations\n')
     h1 = net.addHost('h1', cls=Host, ip='10.0.0.1', defaultRoute=None)
     h2 = net.addHost('h2', cls=Host, ip='10.0.0.2', defaultRoute=None)
     h3 = net.addHost('h3', cls=Host, ip='10.0.0.3', defaultRoute=None)
-    sta1 = net.addStation('sta1', ip='10.0.0.4', position='369.0,392.0,0', range=150)
+    sta1 = net.addStation('sta1', ip='10.0.0.4', position='300.0,450.0,0.0', range=300)
 
     info("*** Configuring Propagation Model\n")
     net.setPropagationModel(model="logDistance", exp=4)
@@ -53,9 +53,16 @@ def myNetwork():
     net.addLink(ap1, s4)
     net.addLink(ap2, s3)
     net.addLink(ap3, s5)
+    net.addLink(s1, controller_)
 
     info( '*** Plotting graph\n')
-    net.plotGraph(max_x=4000, max_y=4000)
+    net.plotGraph(max_x=5000, max_y=4000)
+
+    info( '*** Starting mobility\n')
+    net.startMobility(time=0, repetitions=1, ac_method='ssf')
+    net.mobility(sta1, 'start', time=20, position='300.0,2050.0,0.0')
+    net.mobility(sta1, 'stop', time=60, position='4700.0,2050.0,0.0')
+    net.stopMobility(time=61)
 
     # info( '*** Starting controllers\n')
     # for controller in net.controllers:
@@ -63,26 +70,28 @@ def myNetwork():
 
     info( '*** Starting network, controller and switches/APs\n')
     net.build()
+    net.addNAT().configDefault()
     c0.start()
-    net.get('s1').start([c0])
-    net.get('s2').start([c0])
-    net.get('s3').start([c0])
-    net.get('s4').start([c0])
-    net.get('s5').start([c0])
-    net.get('ap1').start([c0])
-    net.get('ap2').start([c0])
-    net.get('ap3').start([c0])
+    s1.start([c0])
+    s2.start([c0])
+    s3.start([c0])
+    s4.start([c0])
+    s5.start([c0])
+    ap1.start([c0])
+    ap2.start([c0])
+    ap3.start([c0])
 
     info( '*** Starting monitor interface and wireshark\n')
+    # sta1.cmd('xterm')
     sta1.cmd('iw dev %s interface add mon0 type monitor' % sta1.params['wlan'][0])
     sta1.cmd('ifconfig mon0 up')
-    sta1.cmd('wireshark -i mon0 &')
-
-    info( '*** Starting mobility\n')
-    net.startMobility(time=0, repetitions=1, ac_method='ssf')
-    net.mobility(sta1, 'start', time=10, position='300.0,450.0,0.0')
-    net.mobility(sta1, 'stop', time=50, position='2700.0,450.0,0.0')
-    net.stopMobility(time=51)
+    c0.cmd('ifconfig hwsim0 up')
+    sta1.cmd('wireshark -i mon0 -k &')
+    c0.cmd('wireshark -i hwsim0 -k &')
+    # c0.cmd('wireshark -i con-eth0 -k &')
+    sta1.cmd('xterm -hold -e python sta1.py')
+    # c0.cmd('./sta1.py &')
+    # c0.cmd('gnome-terminal -- python sta1.py')
 
     info( '*** Running CLI\n')
     CLI(net)
--- sta1.py
+++ sta1.py
@@ -2,32 +2,33 @@
 from scapy.all import *
 import os
 # import time
+# os.popen("gnome-terminal")
 
 def pkt_callback(pkt):
     # if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 8:
     #     print("Beacon")
     if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 0:
         print("Association Request")
-        msg = "%d" % (pkt.subtype) #subtype: 1
-        packet = Ether()/IP(src="10.0.0.4", dst="127.0.0.1")/TCP(sport=8000, dport=6653)/msg
+        msg = "%d" % (pkt.subtype) #subtype: 0
+        packet = Ether()/IP(src="10.0.0.4", dst="10.0.0.100")/TCP(sport=8000, dport=6653)/msg
         send(packet, verbose=0)
 
     if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 10:
         print("Disassociation")
-        msg = "%d" % (pkt.subtype) #subtype: 1
-        packet = Ether()/IP(src="10.0.0.4", dst="127.0.0.1")/TCP(sport=8000, dport=6653)/msg
+        msg = "%d" % (pkt.subtype) #subtype: 10
+        packet = Ether()/IP(src="10.0.0.4", dst="10.0.0.100")/TCP(sport=8000, dport=6653)/msg
         send(packet, verbose=0)
 
     if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 12:
         print("Disauthentication")
-        msg = "%d" % (pkt.subtype) #subtype: 1
-        packet = Ether()/IP(src="10.0.0.4", dst="127.0.0.1")/TCP(sport=8000, dport=6653)/msg
+        msg = "%d" % (pkt.subtype) #subtype: 12
+        packet = Ether()/IP(src="10.0.0.4", dst="10.0.0.100")/TCP(sport=8000, dport=6653)/msg
         send(packet, verbose=0)
 
     if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 1:
         print("Association Response")
         msg = "%d" % (pkt.subtype) #subtype: 1
-        packet = Ether()/IP(src="10.0.0.4", dst="127.0.0.1")/TCP(sport=8000, dport=6653)/msg
+        packet = Ether()/IP(src="10.0.0.4", dst="10.0.0.100")/TCP(sport=8000, dport=6653)/msg
         send(packet, verbose=0)
         
         # f = open('/home/wifi/ACN/Project/alert.txt', 'w+')
